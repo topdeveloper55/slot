4dd851793acfbebe3652cd01ccd38da51b23bb88
nicolassoft94 <nicolas.soft94@gmail.com>
2023-08-14 17:02:35 +0900

    changefix

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
     <div
       className={`absolute h-full w-full md:overflow-hidden ${poppins.variable} font-sans`}
     >
-      <iframe className="h-full w-full absolute" src="1.html"></iframe>
+      <iframe className="h-full w-full fixed" src="1.html"></iframe>
       <div className="flex fixed top-5 left-10 z-10">
         <button>
           <img src="/menu.png" className="h-4/5"/>
@@ -45,15 +45,15 @@ export default function Home() {
           <img src="/balance.png" className="h-4/5"/>
         </div>
       </div>
-      <div className="flex fixed w-full h-full items-center justify-center z-10">
+      <div className="flex fixed w-full h-full items-center justify-center">
         <div className="h-[100%]">
           <img src="/slot mchn.png" className="h-full w-full"/>
         </div>
       </div>
       <div className="flex fixed bottom-4 right-0 z-10">
-        <div>
+        <button>
           <img src="/spin.png" className="h-4/5"/>
-        </div>
+        </button>
       </div>
     </div>
   );
